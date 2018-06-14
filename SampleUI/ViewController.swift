65933b901d997479246041c6fd5bb6a159a68a14
m_hasegawa <m_hasegawa@ga-tech.co.jp>
2018-06-14 11:44:59 +0900

    create project

---
+++ SampleUI/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SampleUI
+//
+//  Created by m_hasegawa on 2018/06/14.
+//  Copyright © 2018年 marcy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
